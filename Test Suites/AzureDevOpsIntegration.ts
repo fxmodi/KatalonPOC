a32280b241cc7280ac0fe486a6c47d70da82cd44
HQ\FXModi <fremil.modi@expresspros.com>
2024-04-08 15:08:18 +0530

    only amazon

--- Test Suites/AzureDevOpsIntegration.ts
+++ Test Suites/AzureDevOpsIntegration.ts
@@ -12,33 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ad15174f-a1d8-4806-b4df-9128896746a2</testSuiteGuid>
    <testCaseLink>
-      <guid>575f634a-6dea-40dc-9d81-636fc12c26b5</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/POC/RecordModeDemo</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f84566b6-f49d-4ccd-8833-5c42d450ce50</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/POC/ScriptModeDemo</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>3edc19a9-c267-4c0d-a142-59dbab468bf1</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>44ffee7d-8807-4805-9e99-168698287cca</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <testCaseId>Test Cases/POC/Express_Local</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>43fa734e-e695-4a59-bafa-6e2b6bdd014e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
